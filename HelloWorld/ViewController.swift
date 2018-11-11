89017414ab119032602e222922065f222a430268
Dave Lin <dave101326@gmail.com>
2018-11-11 17:23:39 +0800

    first update, add one ban, one label

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by davelin on 2018/11/11.
+//  Copyright © 2018 davelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBAction func btn(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
